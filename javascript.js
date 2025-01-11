c5634463399ba3ae88e5949adabb0ddf45f8525b
Haileab <womahaileab1428@gmail.com>
2025-01-12 00:56:24 +0300

    Added All Basic Functionality for Calculator Operations

--- javascript.js
+++ javascript.js
@@ -1,37 +1,77 @@
 let numbers = document.querySelector(".numbers");
 let operations = document.querySelector(".operations");
 let onscreen = document.querySelector(".onscreen");
+let clear = document.querySelector(".clear");
+let toggle = document.querySelector(".toggle");
+let percentile = document.querySelector(".percentile");
 let isSecondNumber = false;
 let firstNumber = "";
 let secondNumber = "";
 let result = "";
+let operationsvalue = false;
+let isPoint = false;
+let currentOperations = null;
 function AddNumbers(a, b){
      result = a + b;
-    onscreen.textContent = result;
+    onscreen.textContent = parseFloat(result).toFixed(2);
     firstNumber = result;
     secondNumber = "";
+    currentOperations = null;
 }
 function SubNumbers(a, b){
      result = a - b;
-    onscreen.textContent = result;
+    onscreen.textContent = parseFloat(result).toFixed(2);
     firstNumber = result;
     secondNumber = "";
+    currentOperations = null;
 }
 function MultipleNumbers(a, b){
      result = a * b;
-    onscreen.textContent = result;
+    onscreen.textContent = parseFloat(result).toFixed(2);
     firstNumber = result;
     secondNumber = "";
+    currentOperations = null;
 }
 function DivNumbers(a, b){
-    result = a / b;
-    onscreen.textContent = result;
-    firstNumber = result;
-    secondNumber = "";
+    if(b == 0){
+        onscreen.textContent = `👀`;
+        firstNumber = "";
+        secondNumber = "";
+        result = "";
+    }
+    else{
+        result = a / b;
+        onscreen.textContent = parseFloat(result).toFixed(2);
+        firstNumber = result;
+        secondNumber = "";
+        currentOperations = null;
+    }
+    
+}
+function EqualNumbers(a, b){
+    if(firstNumber && secondNumber && currentOperations){
+        switch(currentOperations){
+            case '*':
+                MultipleNumbers(a, b);
+            case '/':
+                DivNumbers(a, b);
+            case '+':
+                SubNumbers(a, b);
+            default:
+                AddNumbers(a, b);
+        }
+    }
 }
 function ButtonClick(e){
     if(onscreen.textContent == ""){
         onscreen.textContent = e.target.id;
+    }
+    else if(isPoint == true && e.target.id == "."){
+        onscreen.textContent = `${onscreen.textContent}`;
+    }
+    else if(e.target.id == "."){
+        isPoint = true;
+        onscreen.textContent = `${onscreen.textContent}${e.target.id}`;
     }
     else if (isSecondNumber){
         onscreen.textContent = "";
@@ -44,6 +84,16 @@ function ButtonClick(e){
 }
 function StartOperations(e){
     isSecondNumber = true;
+    currentOperations = e.target.id;
+  // Select all divs inside the operations container
+let divs = operations.querySelectorAll("div");
+for(let i = 0; i < divs.length; i++){
+    let h1 = divs[i].querySelector("h1");
+    if(h1.id == e.target.id){
+        operationsvalue = !operationsvalue;
+        divs[i].style.backgroundColor = operationsvalue ? '#e87a1f': '#FF8F1F';
+    }   
+}
     if(!firstNumber){
         firstNumber = onscreen.textContent;
     }
@@ -51,19 +101,41 @@ function StartOperations(e){
     if(secondNumber){
         switch (operator){
             case "*":
-                MultipleNumbers(parseInt(firstNumber), parseInt(secondNumber));
+                MultipleNumbers(parseFloat(firstNumber), parseFloat(secondNumber));
                 break;
             case "/":
-                DivNumbers(parseInt(firstNumber), parseInt(secondNumber));
+                DivNumbers(parseFloat(firstNumber), parseFloat(secondNumber));
                 break;
             case "+":
-                AddNumbers(parseInt(firstNumber), parseInt(secondNumber));
+                AddNumbers(parseFloat(firstNumber), parseFloat(secondNumber));
+                break;
+            case "-":
+                SubNumbers(parseFloat(firstNumber),parseFloat(secondNumber));
                 break;
             default:
-                SubNumbers(parseInt(firstNumber), parseInt(secondNumber));
+                EqualNumbers(parseFloat(firstNumber), parseFloat(secondNumber));
         }
     }
 }
-
+function clearScreen(){
+    firstNumber = "";
+    secondNumber = "";
+    result = "";
+    onscreen.textContent = "";
+}
+function toggleValue(){
+    if(onscreen.textContent != 0){
+        onscreen.textContent = -(onscreen.textContent);
+    }
+   
+}
+function calcPercentile(){
+    if(onscreen.textContent != 0){
+        onscreen.textContent = onscreen.textContent / 100;
+    }
+}
 numbers.addEventListener("click", ButtonClick)
-operations.addEventListener("click", StartOperations)
+operations.addEventListener("click", StartOperations);
+clear.addEventListener("click", clearScreen);
+toggle.addEventListener("click", toggleValue);
+percentile.addEventListener("click", calcPercentile);
